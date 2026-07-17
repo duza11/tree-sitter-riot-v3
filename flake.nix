b34044ef0b429ee441e3dc61798b9894532b59e9
duza11 <318.abcdothe.ds@gmail.com>
2026-07-17 23:48:44 +0900

    ci: pin tree-sitter CLI version

--- flake.nix
+++ flake.nix
@@ -3,23 +3,30 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems = [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ];
+      supportedSystems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
-      devShells = forAllSystems (system:
+      devShells = forAllSystems (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
         {
           default = pkgs.mkShell {
-            packages = [
-              pkgs.nodejs_22
-              pkgs.python3
+            packages = with pkgs; [
+              nodejs_22
+              python3
+              tree-sitter
             ];
           };
-        });
+        }
+      );
     };
 }
